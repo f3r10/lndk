9496edc86a3f7fb4e909a63be2876e7d4db68593
f3r10 <frledesma@outlook.com>
2025-08-20 11:22:42 -0500

    adding more time

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -643,7 +643,7 @@ async fn check_pay_offer_with_reconnection(
 
     // Even though LND is up and running, the GRPC service may not. Therefore,
     // an additional time is added to wait for the GRPC service.
-    tokio::time::sleep(Duration::from_millis(100)).await;
+    tokio::time::sleep(Duration::from_millis(500)).await;
 
     // Send another pay_offer process using the same handler.
     // Because of the reconnections, the handler has to be able to connect
